d3c3bdd8ea98a8f9c3883d10c0c443c34ea314c6
rubeen06 <rubendelanieta6@gmail.com>
2024-05-01 18:29:12 +0200

    Diseño terminado al 90%

--- app/src/main/java/com/example/miagenda/ui/tasks/AddTasksFragment.java
+++ app/src/main/java/com/example/miagenda/ui/tasks/AddTasksFragment.java
@@ -1,6 +1,5 @@
-package com.example.miagenda.ui.calendar;
+package com.example.miagenda.ui.tasks;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,14 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.miagenda.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CalendarFragment#newInstance} factory method to
+ * Use the {@link AddTasksFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CalendarFragment extends Fragment {
+public class AddTasksFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +26,7 @@ public class CalendarFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CalendarFragment() {
+    public AddTasksFragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +36,11 @@ public class CalendarFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment calendarFragment.
+     * @return A new instance of fragment AddTasksFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
-        CalendarFragment fragment = new CalendarFragment();
+    public static AddTasksFragment newInstance(String param1, String param2) {
+        AddTasksFragment fragment = new AddTasksFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,34 +57,10 @@ public class CalendarFragment extends Fragment {
         }
     }
 
-
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-
-        // Encuentra el Floating Action Button en tu diseño
-        FloatingActionButton fab = view.findViewById(R.id.fabAgregarEvento);
-
-        // Agrega un OnClickListener al FAB
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Aquí defines la acción que quieres realizar al pulsar el FAB
-                // Por ejemplo, iniciar una nueva actividad
-
-                // Crea un Intent para iniciar la nueva actividad
-                Intent intent = new Intent(getActivity(), AddCalendarFragment.class);
-
-                // Inicia la actividad utilizando el contexto del fragmento
-                getActivity().startActivity(intent);
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_add_tasks, container, false);
     }
 }
