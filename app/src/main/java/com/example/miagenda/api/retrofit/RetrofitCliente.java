1792bc5bdd684e06032e6814e19789777a8596cf
rubeen06 <rubendelanieta6@gmail.com>
2024-05-24 18:40:47 +0200

    margen de abajo notas, eventos y tareas

--- app/src/main/java/com/example/miagenda/api/retrofit/RetrofitCliente.java
+++ app/src/main/java/com/example/miagenda/api/retrofit/RetrofitCliente.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitCliente {
     private static Retrofit retrofit;
     //la url de la api en local
-    private static final String BASE_URL = "http://192.168.18.15:3000";
+    private static final String BASE_URL = "http://192.168.1.233:3000";
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
