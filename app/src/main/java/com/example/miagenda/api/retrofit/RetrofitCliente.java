3b5701b2e07463fc53af1636878b5399ff6452ec
IbrahimBenaichi <ibravmben55@gmail.com>
2024-05-27 18:45:28 +0200

    boton evento borrar

--- app/src/main/java/com/example/miagenda/api/retrofit/RetrofitCliente.java
+++ app/src/main/java/com/example/miagenda/api/retrofit/RetrofitCliente.java
@@ -5,7 +5,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitCliente {
     private static Retrofit retrofit;
     //la url de la api en local
-    private static final String BASE_URL = "http://192.168.1.233:3000";
+    // ip ibra: 192.168.18.15
+    //ip ruben: 192.168.1.233
+    private static final String BASE_URL = "http://192.168.18.15:3000";
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
